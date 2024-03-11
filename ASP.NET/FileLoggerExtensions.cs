ea951d3303e91efe4db4eec342c71f07f50e984e
Stepanchuk Dmytro <dimastep71@gmail.com>
2024-03-11 21:03:32 +0200

    init mvc ASP.NET core project

--- ASP.NET/FileLoggerExtensions.cs
+++
@@ -1,8 +0,0 @@
-﻿public static class FileLoggerExtensions
-{
-    public static ILoggingBuilder AddFile(this ILoggingBuilder builder, string filePath, LogLevel minimumLogLevel = LogLevel.Trace)
-    {
-        builder.AddProvider(new FileLoggerProvider(filePath, minimumLogLevel));
-        return builder;
-    }
-}
--- ASP.NET/Program.cs
+++ ASP.NET/Program.cs
@@ -1,55 +1,25 @@
-using Microsoft.Extensions.Logging;
-
-
-
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+builder.Services.AddRazorPages();
 
-builder.Logging.AddFile($"logs/myapp-{DateTime.UtcNow}.txt".Replace(":", "_"), LogLevel.Error);
-
-
-//.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt")));
 var app = builder.Build();
 
-app.MapGet("/", async (context) =>
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    context.Response.ContentType = "text/html; charset=utf-8";
-    await context.Response.SendFileAsync("html/index.html");
-});
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
 
-app.MapPost("/check-cookies", async (context) =>
-{
-   
+app.UseHttpsRedirection();
+app.UseStaticFiles();
 
-    try
-    {
-        var form = await context.Request.ReadFormAsync();
+app.UseRouting();
 
-        var name = form["name"];
-        var dateTime = form["datetime"];
-        throw new InvalidOperationException("This is a test exception");
+app.UseAuthorization();
 
-        if (context.Request.Cookies.ContainsKey("name"))
-        {
-            await context.Response.WriteAsync($"Hello {name}!");
-        }
-        else
-        {
-            context.Response.Cookies.Append("name", name, new CookieOptions
-            {
-                Expires = Convert.ToDateTime(dateTime)
-            });
-
-            await context.Response.WriteAsync("Hello World!");
-        }
-    }
-    catch (InvalidOperationException ex)
-    {
-        var path = context.Request.Path;
-        app.Logger.LogError($"{path}\n{ex.Message}\n{ex.Data}\n{ex.Source}\n\n");
-    }
-});
+app.MapRazorPages();
 
 app.Run();
-
