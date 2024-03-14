b92283821e79bfbc92d7fdb1dc89a0c1af497c8c
Stepanchuk Dmytro <dimastep71@gmail.com>
2024-03-14 11:02:08 +0200

    lr7.0.2

--- ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
+++ ASP.NET/obj/Debug/net8.0/ASP.NET.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc442718f7935c62d0aece5020e5fbdc46e0f1db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1d065a3c67af745576f54e7fb8e6a2ec2b8984d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ASP.NET")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
