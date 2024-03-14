6362024d34d859c18016982c5ac45f724750b511
Stepanchuk Dmytro <dimastep71@gmail.com>
2024-03-14 10:36:11 +0200

    lr7.0.1

--- ASP.NET/FileUtils.cs
+++ ASP.NET/FileUtils.cs
@@ -8,15 +8,12 @@ public static class FileUtils
         using (StreamWriter writer = new StreamWriter(ms, Encoding.UTF8, 1024, true))
         {
             writer.WriteLine(str);
-            writer.Flush(); // Очистить буфер и записать данные в поток
+            writer.Flush(); 
         }
     }
     public static string RemoveInvalidFileNameChars(string fileName)
         {
-            // Получаем массив недопустимых символов в названии файла
             char[] invalidChars = Path.GetInvalidFileNameChars();
-
-            // Удаляем все недопустимые символы из названия файла
             foreach (char invalidChar in invalidChars)
             {
                 fileName = fileName.Replace(invalidChar.ToString(), "");
